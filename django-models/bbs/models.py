86453bf29a788420433fb9d63a42163d2dc553f6
Joe Donghee Cho <recordingbetter@gmail.com>
2017-06-03 14:41:51 +0900

    adjust bbs models

--- django-models/bbs/models.py
+++ django-models/bbs/models.py
@@ -4,7 +4,7 @@ from django.utils import timezone
 
 
 class Title(models.Model):
-    title = models.CharField(max_length = 30)
+    title = models.CharField(max_length = 50)
     created_date = models.DateTimeField(default = timezone.now)
     author = models.ForeignKey('auth.User')
 
@@ -16,9 +16,9 @@ class Article(models.Model):
     title = models.ForeignKey(Title, on_delete = models.CASCADE, blank = False, null = False)
     subtitle = models.CharField(max_length = 100)
     created_date = models.DateTimeField(default = timezone.now)
-    description1 = models.TextField()
-    examples = models.TextField()
-    description2 = models.TextField()
+    description1 = models.TextField(blank = True, null = True)
+    examples = models.TextField(blank = True, null = True)
+    description2 = models.TextField(blank = True, null = True)
 
     def __str__(self):
         return self.subtitle
