07789c865cc72d3f3b13dda3a2540aa38ed1caa1
Joe Donghee Cho <recordingbetter@gmail.com>
2017-06-04 04:34:33 +0900

    add article_add.html, view, url

--- django-models/bbs/urls.py
+++ django-models/bbs/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'bbs'
 urlpatterns = [
     url(r'^$', views.article_list, name = 'list'),
-    url(r'^(?P<pk>\d+)/$', views.article_detail, name = 'article_detail')
+    url(r'^(?P<pk>\d+)/$', views.article_detail, name = 'article_detail'),
+    url(r'^add/$', views.article_add, name = 'article_add'),
 ]
--- django-models/bbs/views.py
+++ django-models/bbs/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render, get_list_or_404
+from django.contrib.auth import get_user_model
+from django.shortcuts import render, get_list_or_404, redirect
 
 from .models import Article
+
+User = get_user_model()
 
 
 def article_list(request):
     latest_article_list = get_list_or_404(
-        Article.objects.order_by('-created_date')
+        Article.objects.order_by('created_date')
     )
     context = {
         'latest_article_list': latest_article_list,
@@ -14,9 +17,34 @@ def article_list(request):
 
 
 def article_detail(request, pk):
-    article = Article.objects.get(pk=pk)
+    article = Article.objects.get(pk = pk)
     context = {
         'article': article,
         'pk': pk
     }
     return render(request, 'bbs/article_detail.html', context)
+
+
+def article_add(request):
+    if request.method == 'GET':
+        user = get_user_model()
+        context = {
+            'article': Article(),
+        }
+        return render(request, 'bbs/article_add.html', context)
+
+    elif request.method == 'POST':
+        data = request.POST
+        # user = User()
+        print(data)
+        # print('user id:', user)
+        article = Article.objects.create(
+            # author = 1,
+            title_id = 1,
+            subtitle = data['subtitle'],
+            description1 = data['description1'],
+            examples = data['examples'],
+            description2 = data['description2'],
+        )
+        article.save()
+        return redirect('bbs:article_detail', pk = article.pk)
